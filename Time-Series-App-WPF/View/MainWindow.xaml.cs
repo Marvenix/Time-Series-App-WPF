2d39a0d886cb122ffd888a3f39d62a47525d734c
Marvenix <milyczek@gmail.com>
2024-01-03 01:55:47 +0100

    Dodaj pliki projektów.

---
+++ Time-Series-App-WPF/View/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace Time_Series_App_WPF
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
